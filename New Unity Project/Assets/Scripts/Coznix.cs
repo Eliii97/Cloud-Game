945473d3dd80eca3a8bb0a3245514548da9d710f
Eliii97 <88401325+Eliii97@users.noreply.github.com>
2021-08-04 14:13:08 -0700

    MASSIVE UPDATE 2!!!
    Extended level, created Vex, created Coznix. Vexes can bypass IFrames; if  player is without invincibility frames, it deals 2 damage, if they have them, it deals 1. Vexes are stronger, faster versions of Eagles. Coznix spawns a Vex every 8 seconds, Coznix has 20 health. Coznix does not move, and cannot BE moved, and blocks the path forward, requiring the player to defeat him to progress.

--- New Unity Project/Assets/Scripts/Coznix.cs
+++ New Unity Project/Assets/Scripts/Coznix.cs
@@ -2,26 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnailEnemy : MonoBehaviour
+public class Coznix : MonoBehaviour
 {
-    public float speed = 0.0133f;
-    public int eHealth = 1;
     public bool isFollowing = false;
+    public GameObject Vex;
     private SpriteRenderer enemySprite;
+    public int eHealth = 20;
+    public float vexSpawnCounter = 0;
+    public float vexSpawnWaitTime = 8f;
     private Rigidbody2D enemyRb;
     private GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
+        enemyRb = GetComponent<Rigidbody2D>();
+        enemySprite = GetComponent<SpriteRenderer>();
         player = GameObject.Find("dino");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(isFollowing)
-            transform.Translate(Vector3.right * -1 * speed);
+        if (isFollowing)
+        {
+            vexSpawnCounter += Time.deltaTime;
+            if (vexSpawnCounter >= vexSpawnWaitTime)
+            {
+                GameObject rainDrop = Instantiate(Vex, transform);
+                vexSpawnCounter = 0;
+            }
+        }
+
+        if (enemyRb.velocity.x > 0)
+        {
+            enemySprite.flipX = true;
+        }
+        else
+        {
+            enemySprite.flipX = false;
+        }
 
         if (eHealth <= 0)
         {
@@ -32,7 +52,9 @@ public class SnailEnemy : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "dino")
-        { isFollowing = true; }       
+        {
+            isFollowing = true;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -51,5 +73,4 @@ public class SnailEnemy : MonoBehaviour
     {
         eHealth -= eHealthToTake;
     }
-
 }
--- New Unity Project/Assets/Scripts/Vex.cs
+++ New Unity Project/Assets/Scripts/Vex.cs
@@ -2,26 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnailEnemy : MonoBehaviour
+public class Vex : MonoBehaviour
 {
-    public float speed = 0.0133f;
-    public int eHealth = 1;
     public bool isFollowing = false;
-    private SpriteRenderer enemySprite;
+    public float speed = 4;
     private Rigidbody2D enemyRb;
     private GameObject player;
+    private SpriteRenderer enemySprite;
+    public int eHealth = 3;
+    private Game game;
 
     // Start is called before the first frame update
     void Start()
     {
+        game = GameObject.Find("Game").GetComponent<Game>();
         player = GameObject.Find("dino");
+        enemyRb = GetComponent<Rigidbody2D>();
+        enemySprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(isFollowing)
-            transform.Translate(Vector3.right * -1 * speed);
+        if (isFollowing)
+        {
+            Vector2 lookDirection = (player.transform.position - transform.position).normalized;
+            enemyRb.AddForce(lookDirection * speed);
+        }
+
+        if (enemyRb.velocity.x > 0)
+        {
+            enemySprite.flipX = true;
+        }
+        else
+        {
+            enemySprite.flipX = false;
+        }
 
         if (eHealth <= 0)
         {
@@ -32,7 +48,9 @@ public class SnailEnemy : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "dino")
-        { isFollowing = true; }       
+        {
+            isFollowing = true;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -45,11 +63,14 @@ public class SnailEnemy : MonoBehaviour
             enemyRb.isKinematic = true;
             enemyRb.isKinematic = false;
         }
+        if (collision.gameObject.name == "dino")
+        {
+            game.UpdateHealth(1);
+        }
     }
 
     public void UpdateEHealth(int eHealthToTake)
     {
         eHealth -= eHealthToTake;
     }
-
 }
